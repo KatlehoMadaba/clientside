6dd649851acf6b2e828d1b0e7ab33c582812e8ab
KatlehoMadaba <katlehomadaba44@gmail.com>
2024-07-01 15:04:19 +0200

    Making changes to post on github

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import io from 'socket.io-client'
 import {useEffect, useState} from "react";
 const socket=io.connect("http://localhost:3001")
+//Making changes to post on github
 function App() {
   const [room,setRoom]=useState("");
   const [message,setMessage]=useState("");
