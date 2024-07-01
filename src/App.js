728f42cd9b61963c1930dd6f83994c802028b92d
KatlehoMadaba <katlehomadaba44@gmail.com>
2024-07-01 14:33:48 +0200

    Chat room tut

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import io from 'socket.io-client'
 import {useEffect, useState} from "react";
 const socket=io.connect("http://localhost:3001")
 function App() {
+  const [room,setRoom]=useState("");
   const [message,setMessage]=useState("");
   const [messageReceived,setMessageRecieved]=useState("")
+  const joinRoom=()=>{
+    if(room!==""){
+      socket.emit("join_room",room);
+    }
+  };
   const sendMessage=()=>{
     socket.emit("send_message",{message})
   };
@@ -14,8 +20,14 @@ function App() {
     }
   )},[socket]);
   return (
-    
     <div className="App">
+      <input   
+      placeholder='Room Number'
+      onChange={(event)=>{
+        setRoom(event.target.value);
+      }}
+      />
+      <button onClick={joinRoom}>Join roorm</button>
       <input placeholder='Message...' 
       onChange={(event)=>{setMessage(event.target.value)}}/>
       <button onClick={sendMessage}>Send Message</button>
